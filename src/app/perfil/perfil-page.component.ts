e73f853e4672edc8bc80b940d70763ef31bee087
Alfirex <gplp97@gmail.com>
2019-02-07 04:15:11 +0100

    Cambiamos Un poco el contenido del Home,  Remplazamos el formato de HTML5 por etiquetas nativas de Ionic(En la pagina Ventas) y modificamos el Perfil

--- src/app/perfil/perfil-page.component.ts
+++ src/app/perfil/perfil-page.component.ts
@@ -7,11 +7,6 @@ import {Component, ViewChild, OnInit, Renderer, ElementRef} from '@angular/core'
 })
 export class PerfilPage implements OnInit {
 
-    public accordingExapandedeEST = false;
-    public accordingExapandedeAPU = false;
-    @ViewChild('ccc', { read: ElementRef  }) cardContentEST: any;
-    @ViewChild('bbb', { read: ElementRef  }) cardContentAPU: any;
-
     constructor(public renderer: Renderer) {
 
 
@@ -19,36 +14,7 @@ export class PerfilPage implements OnInit {
 
     ngOnInit(): void {
 
-      this.renderer.setElementStyle(this.cardContentEST.nativeElement, 'webkitTransition', 'max-height 500ms');
-      this.renderer.setElementStyle(this.cardContentAPU.nativeElement, 'webkitTransition', 'max-height 500ms');
-    }
-    toogleAccording(valor) {
-        switch (valor) {
-            case 'estudios':
-                if (this.accordingExapandedeEST) {
-                    this.accordingExapandedeEST = false;
-                    this.renderer.setElementStyle(this.cardContentEST.nativeElement, 'max-height', '0px');
-                    this.renderer.setElementStyle(this.cardContentEST.nativeElement, 'padding', '0px 16px');
-                } else {
-                    this.accordingExapandedeEST = true;
-                    this.renderer.setElementStyle(this.cardContentEST.nativeElement, 'max-height', '500px');
-                    this.renderer.setElementStyle(this.cardContentEST.nativeElement, 'padding', '13px 16px');
-                }
-                break;
-            case 'apuntes':
-                if (this.accordingExapandedeAPU) {
-                    this.accordingExapandedeAPU = false;
-                    this.renderer.setElementStyle(this.cardContentAPU.nativeElement, 'max-height', '0px');
-                    this.renderer.setElementStyle(this.cardContentAPU.nativeElement, 'padding', '0px 16px');
-                } else {
-                    this.accordingExapandedeAPU = true;
-                    this.renderer.setElementStyle(this.cardContentAPU.nativeElement, 'max-height', '500px');
-                    this.renderer.setElementStyle(this.cardContentAPU.nativeElement, 'padding', '13px 16px');
-                }
-                break;
-            default:
-            // code block
-        }
 
     }
+
 }
